c2f7aff90090d65d8f246412d9cc4de0adcdeabc
ninetailsrabbit <danieldigitalspace@proton.me>
2024-11-21 13:21:35 +0000

    a wrapper method to pick and remove cards that emit signals and do the repetitive stuff at once

--- components/cards/2D/deck.gd
+++ components/cards/2D/deck.gd
@@ -20,26 +20,6 @@ var current_cards: Array[PlayingCard] = []
 var discard_pile: Array[PlayingCard] = []
 
 
-func number_cards(cards: Array[PlayingCard]) -> Array[PlayingCard]:
-	match deck_type:
-		DeckManager.DeckTypes.Spanish:
-			return cards.filter(func(card: PlayingCard): return card.value > 1 and card.value < 8)
-		DeckManager.DeckTypes.French:
-			return cards.filter(func(card: PlayingCard): return card.value > 1 and card.value < 11)
-	 
-	return []
-
-
-func number_cards_from_suit(suit) -> Array[PlayingCard]:
-	return number_cards(cards_from_suit(suit))
-
-
-func cards_from_suit(suit) -> Array[PlayingCard]:
-	if current_cards_by_suit.has(suit):
-		return current_cards_by_suit[suit]
-	
-	return []
-
 
 func fill(amount_of_jokers: int = 0) -> Deck:
 	current_cards = cards.duplicate()
@@ -59,52 +39,83 @@ func shuffle() -> Deck:
 	shuffled.emit()
 	
 	return self
-
 	
+	
+func pick_card(card: PlayingCard) -> PlayingCard:
+	remove_card(card)
+	
+	picked_card.emit()
+	
+	return card
+
+#region Random picks
 func pick_random_number_card() -> PlayingCard:
-	return number_cards(current_cards).pick_random()
+	return pick_card(number_cards().pick_random())
 
 
 func pick_random_card() -> PlayingCard:
 	if current_cards.is_empty():
 		return null
+	
 		
-	var selected_card: PlayingCard = current_cards.pick_random()
-	
-	remove_card(selected_card)
-	
-	return selected_card
+	return pick_card(current_cards.pick_random())
 	
 
 ## The function parameter is not typed as the suits are separated into different enums
 func pick_random_card_from_suit(suit) -> PlayingCard:
-	if current_cards_by_suit.has(suit):
-		return current_cards_by_suit[suit].pick_random()
+	if current_cards_by_suit.has(suit) and current_cards_by_suit[suit].size() > 0:
+		return pick_card(current_cards_by_suit[suit].pick_random())
 	
 	return null
 
 
 func pick_random_ace() -> PlayingCard:
-	if current_cards_by_suit.is_empty():
+	var aces: Array[PlayingCard] = current_cards.filter(func(card: PlayingCard): return card.is_ace())
+	
+	if aces.is_empty():
 		return null
 		
-	return current_cards_by_suit[current_cards_by_suit.keys().pick_random()][0]
-		
-		
+	return pick_card(aces.pick_random())
+#endregion
+
+
+func number_cards() -> Array[PlayingCard]:
+	return extract_number_cards(current_cards)
+	
+
+func number_cards_from_suit(suit) -> Array[PlayingCard]:
+	return extract_number_cards(cards_from_suit(suit))
+
+
+func extract_number_cards(cards: Array[PlayingCard]) -> Array[PlayingCard]:
+	match deck_type:
+		DeckManager.DeckTypes.Spanish:
+			return cards.filter(func(card: PlayingCard): return card.value > 1 and card.value < 8)
+		DeckManager.DeckTypes.French:
+			return cards.filter(func(card: PlayingCard): return card.value > 1 and card.value < 11)
+	 
+	return []
+
+
+func cards_from_suit(suit) -> Array[PlayingCard]:
+	if current_cards_by_suit.has(suit):
+		return current_cards_by_suit[suit]
+	
+	return []
+
+
 func remove_card(card: PlayingCard):
 	current_cards.erase(card)
-	
-	var card_key = current_cards_by_suit.find_key(card)
-	
-	if card_key != null:
-		current_cards_by_suit.erase(card_key)
+	current_cards_by_suit.erase(current_cards_by_suit.find_key(card))
 	
 	add_to_discard_pile(card)
 	
 
+#region Discard pile
 func add_to_discard_pile(card: PlayingCard) -> void:
 	if not discard_pile.has(card):
 		discard_pile.append(card)
+		discarded_card.emit(card)
 
 
 func extract_from_discard_pile(card: PlayingCard) -> PlayingCard:
@@ -114,3 +125,5 @@ func extract_from_discard_pile(card: PlayingCard) -> PlayingCard:
 		return selected_card
 	
 	return null
+	
+#endregion
